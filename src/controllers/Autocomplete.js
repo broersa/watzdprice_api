67a19fa0870224c01192cc7c90b2c2aa94d77a49
Andre Broers <broersa@Andres-MacBook-Air.local>
2017-03-08 15:29:20 +0100

    autocomlete added

---
+++ src/controllers/Autocomplete.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var conn = require('../dal/pgConnection.js');
+var dal = require('../dal/watzdprice.js');
+var MyError = require('../MyError.js');
+var log = require('../log.js');
+
+module.exports.getautocompleteGET = function getautocompleteGET (req, res, next) {
+  var visitor = log.getLogger();
+  visitor.event('getautocomplete', req.swagger.params.apikey.value, req.swagger.params.q.value, 1, {uid: req.swagger.params.apikey.value, uip: req.headers['x-forwarded-for'] || req.connection.remoteAddress, ua: req.headers['user-agent']}).send();
+  if (req.swagger.params.apikey.value !== 'andre') {
+    res.statusCode = 403;
+    return res.end();
+  } else {
+    conn.execute(function (err, client, done) {
+      if (err) {
+        return next(new MyError('ERROR', 'getautocompleteGET', 'Error', {params: req.swagger.params}, err));
+      }
+      dal.getAutocomplete(client, req.swagger.params.q.value, function (err, words) {
+        if (err) {
+          conn.rollback(client, done);
+          return next(new MyError('ERROR', 'getautocompleteGET', 'Error', {params: req.swagger.params}, err));
+        }
+        conn.commit(client, done);
+        res.statusCode = 200;
+        return res.end(JSON.stringify({words: words}));
+      });
+    });
+  }
+};
--- src/dal/watzdprice.js
+++ src/dal/watzdprice.js
@@ -28,5 +28,47 @@ module.exports = {
         proimage: result.rows[0].proimage
       });
     });
+  },
+  addSearchProducts: function (client, searchProducts, callback) {
+    client.query('insert into searchproducts (sprapikey, sprquery values ($1, $2) returning sprkey', [searchProducts.apikey, searchProducts.query], function (err, result) {
+      if (err) {
+        if (err.code === '23505') {
+          return callback(new MyError('DUPLICATEKEY', 'addSearchProducts', 'Duplicate key error', { searchProducts: searchProducts }, err));
+        }
+        return callback(new MyError('ERROR', 'addSearchProducts', 'Error', { searchProducts: searchProducts }, err));
+      }
+      if (result.rowCount !== 1) {
+        return callback(null, null);
+      }
+      callback(null, result.rows[0].sprkey);
+    });
+  },
+  addGetProduct: function (client, getProduct, callback) {
+    client.query('insert into getproduct (gprapikey, gprproduct values ($1, $2) returning gprkey', [getProduct.apikey, getProduct.product], function (err, result) {
+      if (err) {
+        if (err.code === '23505') {
+          return callback(new MyError('DUPLICATEKEY', 'addGetProduct', 'Duplicate key error', { getProduct: getProduct }, err));
+        }
+        return callback(new MyError('ERROR', 'addGetProduct', 'Error', { getProduct: getProduct }, err));
+      }
+      if (result.rowCount !== 1) {
+        return callback(null, null);
+      }
+      callback(null, result.rows[0].gprkey);
+    });
+  },
+  getAutocomplete: function (client, query, callback) {
+    client.query('select acoword from autocomplete where acoword like $1 order by acoproductcount limit 10', [query + '%'], function (err, result) {
+      if (err) {
+        return callback(new MyError('ERROR', 'getAutocomplete', 'Error', { query: query }, err));
+      }
+      var r = [];
+      if (result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          r.push(result.rows[i].acoword);
+        }
+      }
+      callback(null, r);
+    });
   }
 }
